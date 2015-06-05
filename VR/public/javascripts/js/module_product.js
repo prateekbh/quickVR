af5e9b00caf93cbacb427b1f7ad8ea8e297c4099
Prateek Bhatnagar <prateek.b@flipkart.com>
2015-06-06 04:34:04 +0530

    choose product

--- VR/public/javascripts/js/module_product.js
+++ VR/public/javascripts/js/module_product.js
@@ -1,27 +1,34 @@
-var moduleProduct = function () {
-	function makeProduct(imgPath){
+var moduleProduct = function() {
+    function makeProduct(imgPath) {
         var material = new THREE.MeshLambertMaterial({
-          map: THREE.ImageUtils.loadTexture(imgPath)
+            map: THREE.ImageUtils.loadTexture(imgPath)
         });
-                
-        var cube = new THREE.Mesh(new THREE.BoxGeometry(15, 15, 15), material);
+
+        var cube = new THREE.Mesh(new THREE.BoxGeometry(35, 35, 35), material);
         cube.overdraw = true;
         return cube;
     }
-    function getImagePath(){
+
+    function getImagePath() {
 
     }
-    var y = 55; productGap = 20;
-    //var x = 50, y = 60, z= 80, productGap = 10;
-	this.arrangeProduct = function(prodObj, shelf){
-		var z = shelf.y + 90;
-		for (var i = prodObj.length - 1; i >= 0; i--) {
-			var prod = makeProduct("/imgs/"+ prodObj[i].fsn+"_144.jpg"),x = shelf.x;
-			x = i%2 == 0 ? x - 20 : x + 120;
-			prod.position.set(x,y,z);
-			z = (i-1)%2 == 0 ? z : z - productGap; 
-			scene.add(prod)
-		};
-	};
-	return this;
+    var y = 45;
+    productGap = 45;
+
+    this.arrangeProduct = function(prodObj, shelf) {
+         var z = shelf.y + 240;
+        for (var i = prodObj.length - 1; i >= 0; i--) {
+            var prod = makeProduct("/imgs/" + prodObj[i].fsn + "_144.jpg"),
+                x = shelf.x;
+            x = i % 2 == 0 ? x - 20 : x + 120;
+            prod.position.set(x, y, z);
+            z = (i - 1) % 2 == 0 ? z : z - productGap;
+            prod.userData.fsn=prodObj[i].fsn;
+            prod.userData.prod_id=prodObj[i].id;
+            prod.userData.title=prodObj[i].product_title;
+            prod.userData.cost=prodObj[i].estimated_billing_price;
+            scene.add(prod)
+        };
+    };
+    return this;
 }();
