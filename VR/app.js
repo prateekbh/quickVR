35a56746c16b04eb4980d80def9ab65dd99cf752
Prateek Bhatnagar <prateek.b@flipkart.com>
2015-06-05 21:24:51 +0530

    adding express

--- VR/app.js
+++ VR/app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mustacheExpress = require('mustache-express');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var staticRouter = require('./routes/statics.js');
+var imgRouter = require('./routes/imgs.js');
 
 var app = express();
 
 // view engine setup
+app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'mustache');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +25,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/public",staticRouter);
+app.use('/imgs', imgRouter);
 app.use('/', routes);
-app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
